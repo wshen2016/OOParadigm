644d59e10245fa4a9d8cc5ce725a8d288dad4fd3
Wuwei Shen <wuwei.shen@gmail.com>
2017-01-17 16:36:10 -0500

    This is the first revision. Remove method get_power_via_3leg() to a new class PowerVia3Legs. Separation of the concerns. First step to pursue the absolute simplicity.

--- OOParadigmExample/src/edu/wmich/wshen/ooparadigm/example/Computer.java
+++ OOParadigmExample/src/edu/wmich/wshen/ooparadigm/example/Computer.java
@@ -6,21 +6,33 @@ public class Computer {
 	 */
 	
 	private Boolean on;
+	
+	private PowerVia3Legs power;
+
+	public PowerVia3Legs getPower() {
+		return power;
+	}
+
+	public void setPower(PowerVia3Legs power) {
+		this.power = power;
+	}
 
 	public void turn_on()
-	{  on = get_power_via_3leg();
+	{  on = power.get_power_via_3leg();
 	      if(on)
 	         System.out.println("Power is on");
 	}
 	
-	public Boolean get_power_via_3leg(){
-        System.out.println("Power is generated");
-        return true;
-    }
+//	public Boolean get_power_via_3leg(){
+//        System.out.println("Power is generated");
+//        return true;
+//    }
 	
 	public static void main(String[] args)
 	{
 		Computer c = new Computer();
+		PowerVia3Legs p = new PowerVia3Legs();
+		c.setPower(p);
 		c.turn_on();
 	}
 
---
+++ OOParadigmExample/src/edu/wmich/wshen/ooparadigm/example/PowerVia3Legs.java
@@ -0,0 +1,10 @@
+package edu.wmich.wshen.ooparadigm.example;
+
+public class PowerVia3Legs {
+	
+	public Boolean get_power_via_3leg(){
+      System.out.println("Power is generated");
+      return true;
+    }
+
+}
